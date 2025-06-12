20d33e539eaeb11a0db6bd3f852e80b769322391
Josué Salinas <josuesalinass2207@gmail.com>
2025-06-12 18:25:29 -0500

    Creé la clase Usuario

---
+++ src/Clases/Usuario.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author Josue
+ */
+public class Usuario {
+    
+    private String nombre;
+    private String correo;
+    private String clave;
+    
+    public Usuario(String nombre, String correo, String clave){
+        this.nombre = nombre;
+        this.correo = correo;
+        this.clave = clave;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public String getCorreo(){
+        return correo;
+    }
+    
+    public void setCorreo(String correo){
+        this.correo = correo;
+    }
+    
+    public String getClave(){
+        return clave;
+    }
+    
+    public void getClave(String clave){
+        this.clave = clave;
+    }
+}
